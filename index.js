b50a102ec862c7ba9f8886f7f51fadaa538edc11
Rafael Gil <rafaelgil83@gmail.com>
2020-07-15 22:59:12 -0400

    - working deployment

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.command('/deploy', async ({command, ack, say}) => {
     await ack();
 
     let reply = 'Command option not recognized, if you need help, you can use "/deploy help" \n';
-    const help = 'Welcome to deployer! \n This tool allows to deploy from slack to github utilizing github actions \n ' +
+    const help = 'Welcome to `deployer`! \n This tool allows to deploy from slack to github utilizing github actions \n ' +
         'To perform a deploy, please follow this structure \n' +
-        '/deploy branch_name environment \n' +
-        'ie: /deploy feature/my-feat staging';
+        '```/deploy branch_name environment``` \n' +
+        'ie: ```/deploy feature/my-feat staging```';
 
     if (command.text === 'help' || command.text === '?') {
         reply = help;
