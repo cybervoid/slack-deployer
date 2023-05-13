a92319bb239406a9206e749c40d146b490cf712a
Rafael Gil <cybervoid@cybervoid.ga>
2023-05-13 03:35:01 -0400

    extracted the modal to its own class to make it more readable

---
+++ src/modal.js
@@ -0,0 +1,77 @@
+exports.deploymentModal = () => {
+    return {
+        "type": "modal",
+        "title": {
+            "type": "plain_text",
+            "text": "Deployer",
+            "emoji": true
+        },
+        "submit": {
+            "type": "plain_text",
+            "text": "Submit",
+            "emoji": true
+        },
+        "close": {
+            "type": "plain_text",
+            "text": "Cancel",
+            "emoji": true
+        },
+        "blocks": [
+            {
+                "type": "input",
+                "element": {
+                    "type": "plain_text_input",
+                    "action_id": "plain_text_input-action"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Branch to deploy:",
+                    "emoji": true
+                }
+            },
+            {
+                "type": "input",
+                "element": {
+                    "type": "static_select",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Select Environment",
+                        "emoji": true
+                    },
+                    "options": [
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "QA",
+                                "emoji": true
+                            },
+                            "value": "value-0"
+                        },
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Staging",
+                                "emoji": true
+                            },
+                            "value": "value-1"
+                        },
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Demo",
+                                "emoji": true
+                            },
+                            "value": "value-2"
+                        }
+                    ],
+                    "action_id": "static_select-action"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Deploy to:",
+                    "emoji": true
+                }
+            }
+        ]
+    }
+}
--- src/slack_interface.js
+++ src/slack_interface.js
@@ -1,3 +1,5 @@
+const {deploymentModal} = require('./modal')
+
 exports.attachSlackInterface = app => {
 
     // Listens to incoming messages that contain "hello"
@@ -36,49 +38,7 @@ exports.attachSlackInterface = app => {
                 // Pass a valid trigger_id within 3 seconds of receiving it
                 trigger_id: body.trigger_id,
                 // View payload
-                view: {
-                    type: 'modal',
-                    // View identifier
-                    callback_id: 'view_1',
-                    title: {
-                        type: 'plain_text',
-                        text: 'Modal title'
-                    },
-                    blocks: [
-                        {
-                            type: 'section',
-                            text: {
-                                type: 'mrkdwn',
-                                text: 'Welcome to a modal with _blocks_'
-                            },
-                            accessory: {
-                                type: 'button',
-                                text: {
-                                    type: 'plain_text',
-                                    text: 'Click me!'
-                                },
-                                action_id: 'button_abc'
-                            }
-                        },
-                        {
-                            type: 'input',
-                            block_id: 'input_c',
-                            label: {
-                                type: 'plain_text',
-                                text: 'What are your hopes and dreams?'
-                            },
-                            element: {
-                                type: 'plain_text_input',
-                                action_id: 'dreamy_input',
-                                multiline: true
-                            }
-                        }
-                    ],
-                    submit: {
-                        type: 'plain_text',
-                        text: 'Submit'
-                    }
-                }
+                view: deploymentModal()
             });
             logger.info(result);
         } catch (error) {
@@ -96,49 +56,7 @@ exports.attachSlackInterface = app => {
                 // Pass a valid trigger_id within 3 seconds of receiving it
                 trigger_id: body.trigger_id,
                 // View payload
-                view: {
-                    type: 'modal',
-                    // View identifier
-                    callback_id: 'view_1',
-                    title: {
-                        type: 'plain_text',
-                        text: 'Modal title'
-                    },
-                    blocks: [
-                        {
-                            type: 'section',
-                            text: {
-                                type: 'mrkdwn',
-                                text: 'Welcome to a modal with _blocks_'
-                            },
-                            accessory: {
-                                type: 'button',
-                                text: {
-                                    type: 'plain_text',
-                                    text: 'Click me!'
-                                },
-                                action_id: 'button_abc'
-                            }
-                        },
-                        {
-                            type: 'input',
-                            block_id: 'input_c',
-                            label: {
-                                type: 'plain_text',
-                                text: 'What are your hopes and dreams?'
-                            },
-                            element: {
-                                type: 'plain_text_input',
-                                action_id: 'dreamy_input',
-                                multiline: true
-                            }
-                        }
-                    ],
-                    submit: {
-                        type: 'plain_text',
-                        text: 'Submit'
-                    }
-                }
+                view: deploymentModal()
             });
             logger.info(result);
         } catch (error) {
