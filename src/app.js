4083bfffc8333d1ba2b9c72dad5f6a2bb5c274a7
Rafael Gil <cybervoid@cybervoid.ga>
2023-05-13 03:06:45 -0400

    added 2 triggers so I can test locally as well

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 const {loadBoltLocal, loadBoltLambda} = require("./bolt_connector");
 
-(async () => {
-    // Start your app
-    const bolt = loadBoltLocal(true)
-    await bolt.start(3000);
-
-    console.log('⚡️ Bolt app is running!');
-})();
-
 exports.handler = async (event, context) => {
     try {
-        return await loadBoltLambda(event, context)
+
+        if (event === undefined) {
+            //initiate bolt in local mode
+            const bolt = loadBoltLocal(true)
+            await bolt.start(3000);
+        } else {
+            return await loadBoltLambda(event, context)
+        }
 
     } catch (error) {
         console.error(error);
