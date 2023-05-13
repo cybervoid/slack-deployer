88773d2b8e5384212d7b8d0529b830be2dbeffeb
Rafael Gil <cybervoid@cybervoid.ga>
2023-05-13 02:32:41 -0400

    refactored bolt for better org

--- src/bolt.js
+++ src/bolt.js
@@ -1,95 +1,115 @@
 const {App} = require('@slack/bolt');
 
 
-const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-});
+exports.loadBolt = (local) => {
 
-// Listens to incoming messages that contain "hello"
-app.message('hello', async ({message, say}) => {
-    // say() sends a message to the channel where the event was triggered
-    await say({
-        blocks: [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": `Hey there <@${message.user}>!`
-                },
-                "accessory": {
-                    "type": "button",
+    let app;
+    if (local === true) {
+        app = new App({
+            token: process.env.SLACK_BOT_TOKEN,
+            signingSecret: process.env.SLACK_SIGNING_SECRET,
+        });
+    } else {
+        //loading from a lambda function
+        const awsLambdaReceiver = new AwsLambdaReceiver({
+            signingSecret: process.env.SLACK_SIGNING_SECRET,
+        });
+
+        app = new App({
+            token: process.env.SLACK_BOT_TOKEN,
+            receiver: awsLambdaReceiver,
+        });
+    }
+
+    // Listens to incoming messages that contain "hello"
+    app.message('hello', async ({message, say}) => {
+        // say() sends a message to the channel where the event was triggered
+        await say({
+            blocks: [
+                {
+                    "type": "section",
                     "text": {
-                        "type": "plain_text",
-                        "text": "Click Me"
+                        "type": "mrkdwn",
+                        "text": `Hey there <@${message.user}>!`
                     },
-                    "action_id": "button_click"
+                    "accessory": {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Click Me"
+                        },
+                        "action_id": "button_click"
+                    }
                 }
-            }
-        ],
-        text: `Hey there <@${message.user}>!`
+            ],
+            text: `Hey there <@${message.user}>!`
+        });
     });
-});
 
 // Listen for a slash command invocation
-app.command('/deploy', async ({ack, body, client, logger}) => {
-    // Acknowledge the command request
-    await ack();
+    app.command('/deploy', async ({ack, body, client, logger}) => {
+        // Acknowledge the command request
+        await ack();
 
-    try {
-        // Call views.open with the built-in client
-        const result = await client.views.open({
-            // Pass a valid trigger_id within 3 seconds of receiving it
-            trigger_id: body.trigger_id,
-            // View payload
-            view: {
-                type: 'modal',
-                // View identifier
-                callback_id: 'view_1',
-                title: {
-                    type: 'plain_text',
-                    text: 'Modal title'
-                },
-                blocks: [
-                    {
-                        type: 'section',
-                        text: {
-                            type: 'mrkdwn',
-                            text: 'Welcome to a modal with _blocks_'
-                        },
-                        accessory: {
-                            type: 'button',
-                            text: {
-                                type: 'plain_text',
-                                text: 'Click me!'
-                            },
-                            action_id: 'button_abc'
-                        }
+        try {
+            // Call views.open with the built-in client
+            const result = await client.views.open({
+                // Pass a valid trigger_id within 3 seconds of receiving it
+                trigger_id: body.trigger_id,
+                // View payload
+                view: {
+                    type: 'modal',
+                    // View identifier
+                    callback_id: 'view_1',
+                    title: {
+                        type: 'plain_text',
+                        text: 'Modal title'
                     },
-                    {
-                        type: 'input',
-                        block_id: 'input_c',
-                        label: {
-                            type: 'plain_text',
-                            text: 'What are your hopes and dreams?'
+                    blocks: [
+                        {
+                            type: 'section',
+                            text: {
+                                type: 'mrkdwn',
+                                text: 'Welcome to a modal with _blocks_'
+                            },
+                            accessory: {
+                                type: 'button',
+                                text: {
+                                    type: 'plain_text',
+                                    text: 'Click me!'
+                                },
+                                action_id: 'button_abc'
+                            }
                         },
-                        element: {
-                            type: 'plain_text_input',
-                            action_id: 'dreamy_input',
-                            multiline: true
+                        {
+                            type: 'input',
+                            block_id: 'input_c',
+                            label: {
+                                type: 'plain_text',
+                                text: 'What are your hopes and dreams?'
+                            },
+                            element: {
+                                type: 'plain_text_input',
+                                action_id: 'dreamy_input',
+                                multiline: true
+                            }
                         }
+                    ],
+                    submit: {
+                        type: 'plain_text',
+                        text: 'Submit'
                     }
-                ],
-                submit: {
-                    type: 'plain_text',
-                    text: 'Submit'
                 }
-            }
-        });
-        logger.info(result);
-    } catch (error) {
-        logger.error(error);
-    }
-});
+            });
+            logger.info(result);
+        } catch (error) {
+            logger.error(error);
+        }
+    });
 
-exports.Bolt = app
+
+    return app;
+}
+
+
+
