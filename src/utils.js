94f08ab8c8f26d2b2d673a01a4193e7f9548453c
Rafael Gil <cybervoid@cybervoid.ga>
2023-05-15 01:37:45 -0400

    add user security check for deployments

--- src/utils.js
+++ src/utils.js
@@ -4,10 +4,11 @@ exports.validateRequest = (event) => {
 
     const decodedEventBody = Buffer.from(event.body, "base64").toString('utf-8')
     const payload = querystring.parse(decodedEventBody)
+
     const user = payload.user_name
     const userId = payload.user_id
-    const allowedUsers = process.env.AllowedUsers
-    console.log(`AllowedUsers:`, allowedUsers)
-    console.log(`First user:`, allowedUsers[0])
-    return false
+
+    const allowedUsers = JSON.parse(process.env.AllowedUsers)
+
+    return (userId === allowedUsers[user])
 }
